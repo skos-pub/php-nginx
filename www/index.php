049a474ec0427f3cf93e769d615788c98774552b
telkomdev-suhandi <suhandi.kosasi@gmail.com>
2024-11-22 13:26:05 +0700

    add SAYHI

--- www/index.php
+++ www/index.php
@@ -1,3 +1,5 @@
 
 <p>Hello world, this is php served from apache inside container: <?php echo gethostname() ?></p>
 <p>Version <?php echo $_ENV["VERSION"];?></p>
+
+<p>HI <b><?php echo $_ENV["SAYHI"];?></b></p>
